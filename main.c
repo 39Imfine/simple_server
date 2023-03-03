562f7034f64ebce4eae5ec6506f9c72ff93a93c3
Harupa <61609035+Haurpa@users.noreply.github.com>
2023-03-03 09:54:19 +0900

    my_addr 구조체 채우기

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#define PORT 3500
+#define BACKLOG 10
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int sockfd, new_fd;
+
+    struct sockaddr_in my_addr;
+    struct sockaddr_in their_addr;
+    int sin_size;
+
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(PORT);
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    return 1;
 }
